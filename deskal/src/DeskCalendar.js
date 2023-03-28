a652b29acd1cff4cc7f74189cb0e364f286b1171
Derek Graham <deejaygraham@gmail.com>
2023-03-28 13:43:07 +0100

    Update DeskCalendar.js

--- deskal/src/DeskCalendar.js
+++ deskal/src/DeskCalendar.js
@@ -3,30 +3,34 @@ import * as DateFunctions from './dates';
 
 
 const DeskCalendar = () => {
-    const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-    const nextDay = () => {
-        console.log("Next Day")
-        setCurrentDate(DateFunctions.addDays(currentDate, 1));
-     }
-     const prevDay = () => {
-        console.log("Previous Day")
-        setCurrentDate(DateFunctions.addDays(currentDate, -1));
-     }
+  const nextDay = () => {
+   // console.log("Next Day")
+    setCurrentDate(DateFunctions.addDays(currentDate, 1));
+  }
+  const prevDay = () => {
+    // console.log("Previous Day")
+    setCurrentDate(DateFunctions.addDays(currentDate, -1));
+  }
 
-    const dayOfWeek = DateFunctions.getDayOfWeek(currentDate);
-    const dayOfMonth = DateFunctions.getDayOfMonth(currentDate);
+  const dayOfWeek = DateFunctions.getDayOfWeek(currentDate);
+  const dayOfMonth = DateFunctions.getDayOfMonth(currentDate);
 
-            /* add buttons to page through - past, today, future */
-            /* can page through, identify past, identify future */
-    return (
-        <div class="calendar">
+  /* add buttons to page through - past, today, future */
+  /* can page through, identify past, identify future */
+  return (
+  <div class="calendar">
             
-            <div class="day-of-week"><button className="previous" onClick={ prevDay }>--</button> { dayOfWeek } <button className="next" onClick={ nextDay }>++</button></div>
-            <hr />
-            <div class="day-of-month">{ dayOfMonth }</div>
-        </div>
-    );
+    <div class="day-of-week">
+      <button id="prev" className="previous" onClick={ prevDay }>--</button> 
+      { dayOfWeek } 
+      <button id="next" className="next" onClick={ nextDay }>++</button>
+    </div>
+    <hr />
+    <div class="day-of-month">{ dayOfMonth }</div>
+    </div>
+);
 };
 
 export default DeskCalendar;
